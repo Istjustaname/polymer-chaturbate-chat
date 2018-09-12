901df1e99401ecfc995c5fcf2e7e917656f893b1
Pascal <freak9191@web.de>
2018-09-12 16:59:30 +0200

    bla

--- chaturbate-chat.js
+++ chaturbate-chat.js
@@ -1,4 +1,4 @@
-import {PolymerElement, html} from '@polymer/polymer';
+import { PolymerElement, html } from '@polymer/polymer/polymer-element.js';
 
 class ChaturbateChat extends PolymerElement {
   static get template() {
